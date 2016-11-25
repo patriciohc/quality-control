b7c5cb309215848857a5684660010deea26b541f
patricio <patriciohc.0@gmail.com>
2016-11-25 16:52:05 -0600

    separacion de controladores

--- www/js/app/controllers.js
+++ www/js/app/controllers.js
@@ -85,44 +85,6 @@ app.controller('uploadExcel', function($scope, $http) {
 
 })
 
-// vista search
-.controller('search', function($scope, $http) {
-
-    $http.get("/api/cat-producto").then(function(response){
-        $scope.productos = response.data;
-        //$scope.attrs = $scope.productos[0].atributos;
-    });
-
-    $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-    $scope.series = ['Series A', 'Series B'];
-
-    $scope.data = [
-        [65, 59, 80, 81, 56, 55, 40],
-        [28, 48, 40, 19, 86, 27, 90]
-    ];
-
-    $scope.attrs = ["SiO2", "Al2O3", "FeO", "CaO"];
-
-    $scope.changeSelectAtrr = function () {
-        //hidenAll();
-        var producto = $scope.productos[$scope.slProducto].name;
-        var atributo = $scope.slAttr;
-        var parms = "?nameProducto=" + producto + "&" + "atributo=" + atributo;
-        $http.get("/api/getAtributo/" + parms).then(function(response) {
-            var dataColumn = response.data;
-            // promedio
-            $scope.promedio = estadistica.getPromedio(dataColumn);
-            // desviacion estandar
-            $scope.desviacion = estadistica.desvStd(dataColumn);
-            // rango
-            $scope.rango = 232;
-            //makeScatterChart(sheet, $scope.slAttr);
-            //makeColumnChart(sheet, $scope.slAttr);
-        });
-    }
-
-})
-
 //.controller('mainControl', function($scope) {
 
 //    $scope.checksTable = [];
