1640c8015d7c84a93c7df2e68914e5641fc7df06
patricio <patriciohc.0@gmail.com>
2016-11-30 23:32:33 -0600

    se agrega grafica de correlacion

---
+++ www/js/app/controllers/correlacion.js
@@ -0,0 +1,52 @@
+app
+// vista correlacion
+.controller('correlacion', function($scope, $http) {
+
+    $scope.correlacion = getConfigScatter();
+
+    $http.get("/api/cat-producto").then(function(response){
+        $scope.productos = response.data;
+    });
+
+    $scope.changeSelectProducto1 = function (){
+        var producto = $scope.productos[$scope.slProducto1];
+
+        var parms = "?nameProducto=" + producto.nombre; // + "&" + "atributo=" + atributo;
+        $http.get("/api/atributo/" + parms).then(function(response) {
+            $scope.datosProducto1 = response.data;
+        });
+    }
+
+    $scope.changeSelectProducto2 = function (){
+        var producto = $scope.productos[$scope.slProducto2];
+
+        var parms = "?nameProducto=" + producto.nombre; // + "&" + "atributo=" + atributo;
+        $http.get("/api/atributo/" + parms).then(function(response) {
+            $scope.datosProducto2 = response.data;
+        });
+    }
+
+    
+    $scope.changeSelectAtrr1 = function () {
+        $scope.datosAtributo1 = $scope.datosProducto1[$scope.slAttr1];
+    }
+    
+    $scope.changeSelectAtrr2 = function () {
+        $scope.datosAtributo2 = $scope.datosProducto2[$scope.slAttr2];
+
+        var x = $scope.datosAtributo1.data.map(function(obj){
+            return [obj.value];
+        });
+
+        var y = $scope.datosAtributo2.data.map(function(obj){
+            return [obj.value];
+        });
+        
+        var xy = []
+        for (var i in x){
+            xy.push(x[i], y[i]);
+        }
+        $scope.correlacion.series[0].data = xy;
+    }
+
+});
