de0e97513fd615a44cb7703174df2ba1f009cb7e
patricio <patriciohc.0@gmail.com>
2016-12-07 21:38:17 -0600

    se agrega grafica circular, y moda

--- server/estadistica.js
+++ server/estadistica.js
@@ -1,32 +1,74 @@
 'use strict'
 
 var estadistica = {
-    // recibe data del tipo 
-    //[{ identificador: "valor", value: valor }, .....]
+    
     getPromedio: function (data){
         var suma = 0;
         for (var i = 0; i < data.length; i++)
-            suma += data[i].value;
-        var promedio = suma / data.length;
-        return Math.round(promedio * 100) / 100;
+            suma += data[i];
+        return suma / data.length;
     },
-    // recibe data del tipo 
-    //[{ identificador: "valor", value: valor }, .....]
+
     desvStd: function (data){
         var N = data.length;
         var x_p = estadistica.getPromedio(data);
         var sumatoria = 0;
         for (var i = 0; i < data.length; i++){
-            var x_i = data[i].value;
+            var x_i = data[i];
             sumatoria += Math.pow( x_i - x_p , 2);
         }
-        var desvStd = Math.sqrt(sumatoria / N);
-        return Math.round(desvStd * 100) / 100;
+        return Math.sqrt(sumatoria / N);
     },
 
     rango: function (data) {
-        return Math.round(data[0] * 100) / 100;
-    }
+        return Math.max.apply(null, data) - Math.min.apply(null, data);
+    },
+
+
+    //Funcion que calcula la moda, si hi han 2 repetits retorna un -1
+    moda: function moda(array){
+ 			//iniciamos las variables necesarias en todo el codigo
+ 			var moda, moda2;
+ 			var contador = 0, contador2 = 0;
+ 			//Recorremos la array
+ 			for (var x=0; x<array.length; x++){
+ 				//Miramos que el numero cogido no sea el de la moda
+ 				if(array[x] != moda) {
+ 					var contadorReinicia=0;
+ 					//Recorremos la array para encontrar concordancias on el numero sacado de la array de X
+ 					for(var i=0; i<array.length; i++){
+ 						//cunado el numero sea igual al de la array de x le añadimos 1 al contador
+ 						if (array[i] == array[x]) contadorReinicia++;
+ 					}
+//si el contador que se reinicia nos da mas alto que el contador general añadimos el numero a la variable moda y cambiamos el contador general por el que reinicia
+ 					if (contadorReinicia>contador){
+ 						contador = contadorReinicia;
+ 						moda = array[x];
+ 					}
+ 				}
+ 			}
+ 			//Miramos que no hayan 2 con la misma cantidad
+ 			for ( var x=0; x<array.length; x++ ){
+ 				//Miramos que el numero cogido no sea el de la moda
+ 				if(array[x] != moda && array[x] != moda2){
+ 					var contadorReinicia=0;
+ 					//Recorremos la array para encontrar concordancias on el numero sacado de la array de X
+ 					for(var i=0; i<array.length; i++){
+ 						//cunado el numero sea igual al de la array de x le añadimos 1 al contador
+ 						if (array[i] == array[x]) contadorReinicia++;
+ 					}
+ 					//si el contador que se reinicia nos da mas alto que el contador general añadimos el numero a la variable moda y cambiamos el contador general por el que reinicia
+ 					if (contadorReinicia>contador2){
+ 						contador2 = contadorReinicia;
+ 						moda2 = array[x];
+ 					}
+ 					//Si tenemos 2 de la misma cantidad retornamos -1
+ 					if (contador2 == contador) return -1;
+ 				}
+ 			}
+ 			//Retornamos la moda!!!
+ 			return moda;
+ 		}
 
 }
 
--- www/js/app/controllers/general.js
+++ www/js/app/controllers/general.js
@@ -3,16 +3,14 @@ app
 .controller('general', function($scope, $http) {
     $http.get('/api/atributo').then(function(response){
         $scope.productos = response.data;
-        
+        $scope.pieChart = new Array($scope.productos.length);
         for (var i = 0; i < $scope.productos.length; i++){
             var config = getConfigPie();
             var producto = $scope.productos[i];
             for (var j in producto.data){
                 config.series[0].data.push({name: producto.data[j].atributo, y: producto.data[j].promedio});
             }
-            $scope.$apply(function(){
-                $scope["pieChart_" + i] = config;
-            });
+            $scope.pieChart[i] = config;
         }
     });
 });
