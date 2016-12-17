e93aca51110c90cb26753bccbab7300c59206d01
patricio <patriciohc.0@gmail.com>
2016-12-17 11:45:02 -0600

    correciones en menu

--- www/js/app/controllers/menu.js
+++ www/js/app/controllers/menu.js
@@ -5,26 +5,25 @@ app.controller('menu', function($scope, $state) {
         var texto = $scope.txtBusqueda;
         $state.go("menu.search", {texto: texto});
     }
-
-    /* $(function() {
-        var offset = $("#sidebar-wrapper").offset();
-        var topPadding = 15;
-        $(window).scroll(function() {
-        if ($("#sidebar-wrapper").height() < $(window).height() && $(window).scrollTop() > offset.top) { /* LINEA PARA NO ANIMAR SI EL Menu ES MAYOR AL TAMANO DE PANTALLA
-        $("#sidebar-wrapper").stop().animate({
-        marginTop: $(window).scrollTop() - offset.top + topPadding
-        });
-        } else {
-        $("#sidebar-wrapper").stop().animate({
-        marginTop: 0
-        });
-        };
-        });
-
-    }) */
-
     toggle();
 });
 
 
 
+function toggle(){
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+
+    $("#menu-toggle-2").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled-2");
+        $('#menu ul').hide();
+    });
+
+    $(".itemMenu").click(function(e){
+          $(".itemMenu").removeClass("active");
+          $(this).addClass("active");
+    });
+}
